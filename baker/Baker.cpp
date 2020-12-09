85c2234ff5dfe46b6c9f7f246539fe28ce40c452
tthinh188 <thinh.phan.18@cnu.edu>
2020-12-08 23:13:46 -0500

    update

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -18,12 +18,15 @@ void Baker::bake_and_box(ORDER &anOrder) {
 	Box aBox;
 	int numDonutInBox = 0;
 	for (int i = 0; i < anOrder.number_donuts; i++) {
+
+		PRINT3("Baker ", id, " puts 1 donut into box");
 		numDonutInBox++;
 		DONUT donut;
 		aBox.addDonut(donut);
 
 		// > 12 donuts, push a box into vector, then clear.
 		if (numDonutInBox % DOZEN == 0) {
+			PRINT1("Switching box");
 			anOrder.boxes.push_back(aBox);
 			aBox.clear();
 		}
@@ -37,22 +40,21 @@ void Baker::bake_and_box(ORDER &anOrder) {
 }
 
 void Baker::beBaker() {
-	std::unique_lock<mutex> lck(mutex_order_inQ);
-	while(!b_WaiterIsFinished && order_in_Q.empty()) {
-		cv_order_inQ.wait(lck);
-	}
 
 	while(true) {
+		std::unique_lock<mutex> lck(mutex_order_inQ);
+
+		while(!b_WaiterIsFinished && order_in_Q.empty()) {
+			cv_order_inQ.wait(lck);
+		}
+
 		if(!order_in_Q.empty()) {
 			ORDER order = order_in_Q.front();
-			{
-				lock_guard<mutex> lck(mutex_order_outQ);
-				order_in_Q.pop();
-			}
+			order_in_Q.pop();
+			lck.unlock();
 			bake_and_box(order);
 			order_out_Vector.push_back(order);
 		}
-
 		if (b_WaiterIsFinished && order_in_Q.empty()) {
 			break;
 		}
--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -25,17 +25,25 @@ int Waiter::getNext(ORDER &anOrder){
 void Waiter::beWaiter() {
 
 	int next = SUCCESS;
+//	b_WaiterIsFinished = false;
 	//gets new orders until getNext runs out, then signals b_WaiterIsFinished
 	while (next == SUCCESS){
 		ORDER temp;
 		next = getNext(temp);
 		if (next == SUCCESS){
-			lock_guard<mutex> lck(mutex_order_outQ);
+			unique_lock<mutex> lck(mutex_order_inQ);
+			PRINT4("Be waiter ", id, " pushing order number: ", temp.order_number);
 			order_in_Q.push(temp);
-		}else {
-			b_WaiterIsFinished = true;
+		} else {
+			{
+				unique_lock<mutex> lck(mutex_order_inQ);
+				b_WaiterIsFinished = true;
+				PRINT3("Waiter ", id ," notify baker");
+			}
+			cv_order_inQ.notify_all();
 		}
 		//signals Bakers that a new order is in queue or waiter is done
+		PRINT3("Waiter ", id ," notify baker");
 		cv_order_inQ.notify_all();
 
 	}
