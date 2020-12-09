0f33f3ae12babd5ec515fb8bad966d7115ef31f0
codydb <codydb@live.com>
2020-12-08 17:22:59 -0800

    waiter.cpp

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include "stdlib.h"
+#include <externs.h>
 
 #include "../includes/waiter.h"
 
 using namespace std;
+
+mutex waiterMutex;
 
 Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
 }
@@ -14,9 +17,27 @@ Waiter::~Waiter()
 
 //gets next Order(s) from file_IO
 int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+	//lock guard so no two waiters grab the same order
+	lock_guard<mutex> lock(waiterMutex);
+	return myIO.getNext(anOrder);;
 }
 
 void Waiter::beWaiter() {
+
+	int next = SUCCESS;
+	//gets new orders until getNext runs out, then signals b_WaiterIsFinished
+	while (next == SUCCESS){
+		ORDER temp;
+		next = getNext(temp);
+		if (next == SUCCESS){
+			order_in_Q.push(temp);
+		}else {
+			b_WaiterIsFinished = true;
+		}
+		//signals Bakers that a new order is in queue or waiter is done
+		cv_order_inQ.notify_all();
+
+	}
+
 }
 
